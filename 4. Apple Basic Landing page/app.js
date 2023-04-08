0303999d649fe6e6e1d710a23862274d687b93b7
Mohamed Khalil Assaddiki <123842557+MohamedKhalilAssa@users.noreply.github.com>
2023-04-08 14:50:31 +0000

    Add files via upload

---
+++ 4. Apple Basic Landing page/app.js
@@ -0,0 +1,8 @@
+let container=document.querySelector("#container");
+let image = document.getElementById("product");
+
+function Switch(Phone, background){
+    image.src=Phone;
+    container.style.background=background;
+}
+
